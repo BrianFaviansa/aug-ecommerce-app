8548290f9fcd320d31cd7b98466afb6a174c4f5f
BrianFaviansa <brianfpd31@gmail.com>
2024-08-04 21:33:47 +0700

    feat: api get order & get current user order

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -4,7 +4,7 @@ import Order from "../models/orderModel.js";
 
 export const createOrder = asyncHandler(async (req, res) => {
   const { email, firstName, lastName, phone, cartItem } = req.body;
-  if (!cartItem || cartItem.length < 0) {
+  if (!cartItem || cartItem.length < 1) {
     res.status(400);
     throw new Error("Your cart is empty");
   }
@@ -46,17 +46,27 @@ export const createOrder = asyncHandler(async (req, res) => {
   });
 });
 export const getAllOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find();
+
   return res.status(201).json({
     message: "All Orders retrieved sucessfully",
+    orders,
   });
 });
 export const getSingleOrder = asyncHandler(async (req, res) => {
+  const orderId = req.params.id;
+  const order = await Order.findById(orderId);
+
   return res.status(201).json({
     message: "Order retrieved sucessfully",
+    order,
   });
 });
 export const getUserOrder = asyncHandler(async (req, res) => {
+  const order = await Order.find({ user: req.user.id });
+
   return res.status(201).json({
     message: "Current user orders retrieved sucessfully",
+    order,
   });
 });
